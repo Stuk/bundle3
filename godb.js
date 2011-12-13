83f02dd4bc9243c7d167dd586e6313f7e4d62fd7
Stuart Knightley <stuart@stuartk.com>
2011-12-13 10:35:13 -0800

    Update godb

--- godb.js
+++ godb.js
@@ -10,25 +10,47 @@ var GoDB = (function() {
   // database, but as a small one with less than 100 rows.
   var processDataTable = function (table) {
     // Get all the column names.
-    // TODO If none of the columns have labels we should assume that the
-    // tabel hasn't been set up properly and that the first row actually
-    // contains the labels.
-    var cols = [];
-    var m = table.getNumberOfColumns();
-    var i, j;
-    for (i = 0; i < m; i++) {
-      cols.push(table.getColumnLabel(i) || table.getColumnId(i));
-    }
 
-    // Map the row data to the column names
-    m = table.getNumberOfRows();
-    var data = [];
-    for (i = 0; i < m; i++) {
+    // Array of column labels
+    var cols = [],
+    // Array of rows in the data tabel. Return variable.
+    data = [],
+    n_cols = table.getNumberOfColumns(), n_rows = table.getNumberOfRows(),
+    // Store how many columns don't have Labels
+    misses = 0,
+    // If there are no labels, then relabel from the first row. (Technically
+    // we don't need this variable, but it makes the code easier to read.
+    relabel = false,
+    i, j;
+
+    for (i = 0; i < n_cols; i++) {
+      // Note the use of the comma operator here to increment the number of
+      // label misses, while still returning the column id.
+      cols.push(table.getColumnLabel(i) || (misses++, table.getColumnId(i)));
+    }
+    // If none of the columns have labels we should assume that the
+    // table hasn't been set up properly and that the first row actually
+    // contains the labels.
+    relabel = misses === n_cols;
+
+    // Map the row values to the column names
+    for (i = 0; i < n_rows; i++) {
       var row = {};
-      for (j = 0; j < cols.length; j++) {
-        row[cols[j]] = table.getValue(i, j);
+      for (j = 0; j < n_cols; j++) {
+        // Use the first row as labels if we need to
+        // TODO: Move this into its own loop?
+        if (relabel && i === 0) {
+          cols[j] = table.getValue(i, j);
+        } else {
+          row[cols[j]] = table.getValue(i, j);
+        }
       }
       data.push(row);
+    }
+
+    // If we've relabeled the first row is useless.
+    if (relabel) {
+      data.shift();
     }
 
     return data;
@@ -44,7 +66,7 @@ var GoDB = (function() {
       }
 
       google.load("visualization", "1", {callback: function() {
-        // TODO support multiple sheets by changing the gid. Sadly it seems
+        // TODO Sadly it seems
         // extra sheets don't have the labels set correctly, even if the
         // first row is frozen. So either we need to guess-correct this,
         // find out how to set labels in Google Docs, or find some other
@@ -95,28 +117,28 @@ var GoDB = (function() {
       // Replace each template script with the HTML that it generates, wrapped in
       // a div. The div element inherits all the attributes set on the template
       // element.
-      // TODO replace underscore.each here with regular JS loops
-      _.each(document.body.querySelectorAll('script[type="text/html"]'),
-        function(tmpl) {
-          // Generate the HTML.
-          var html = templater(tmpl.innerHTML, context);
+      var templates = document.body.querySelectorAll('script[type="text/html"]')
+      for (var i = 0; i < templates.length; i++) {
+        var tmpl = templates[i];
+        // Generate the HTML.
+        var html = templater(tmpl.innerHTML, context);
 
-          // Create the wrapping div and set the attributes except the type
-          // attribute.
-          var container = document.createElement("div");
-          container.innerHTML = html;
-          _.each(tmpl.attributes, function(attr) {
-            if (attr.name !== "type") {
-              container.setAttribute(attr.name, attr.value);
-            }
-          });
-
-          var parent = tmpl.parentElement;
-          // Insert the div and remove the template script.
-          parent.insertBefore(container, tmpl);
-          parent.removeChild(tmpl);
+        // Create the wrapping div and set the attributes except the type
+        // attribute.
+        var container = document.createElement("div");
+        container.innerHTML = html;
+        for (var j = 0; j < tmpl.attributes.length; j++) {
+          var attr = tmpl.attributes[j];
+          if (attr.name !== "type") {
+            container.setAttribute(attr.name, attr.value);
+          }
         }
-      );
+
+        var parent = tmpl.parentElement;
+        // Insert the div and remove the template script.
+        parent.insertBefore(container, tmpl);
+        parent.removeChild(tmpl);
+      }
     }
   };
 }());
